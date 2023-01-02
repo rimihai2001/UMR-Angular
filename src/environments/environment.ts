0dfe0eb005a3b0da5aafa9dbc855ced753f600f7
Radu-Ioan Mihai <rimihai2001@gmail.com>
2023-01-02 16:53:27 +0200

    Firebase connection done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'umr-frontend',
+    appId: '1:609241148254:web:324775a2c084f1ad783dcf',
+    databaseURL: 'https://umr-frontend-default-rtdb.firebaseio.com',
+    storageBucket: 'umr-frontend.appspot.com',
+    apiKey: 'AIzaSyCAgCt2OSzxAsjrU9Vpg9jzEuiUzer1Eqs',
+    authDomain: 'umr-frontend.firebaseapp.com',
+    messagingSenderId: '609241148254',
+    measurementId: 'G-B69EV1ZPKZ',
+  },
   production: false
 };
 
