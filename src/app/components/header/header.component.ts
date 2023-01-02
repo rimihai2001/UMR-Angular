6436cec47bbc559c8e1033a534e6a2452e677d04
Radu-Ioan Mihai <rimihai2001@gmail.com>
2023-01-03 00:35:16 +0200

    Header done

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor(public afAuth: AngularFireAuth, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  logout(): void {
+    this.afAuth.signOut();
+    this.router.navigate(['../login'])
+  }
+
+
+}
