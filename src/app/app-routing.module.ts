cc60636ec0a57711e3fee4f53bc02a7690a31ecd
Radu-Ioan Mihai <rimihai2001@gmail.com>
2023-01-02 18:07:52 +0200

    Auth Routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { AuthGuard } from './services/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomepageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'main', component: MainPageComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '/home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
