6f2cf445cef9f02687f0aac2086e58238e522354
Radu-Ioan Mihai <rimihai2001@gmail.com>
2023-01-03 01:28:29 +0200

    Routing updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddConcertPageComponent } from './pages/add-concert-page/add-concert-page.component';
+import { AllConcertsPageComponent } from './pages/all-concerts-page/all-concerts-page.component';
 import { HomepageComponent } from './pages/home-page/homepage.component';
 import { LoginComponent } from './pages/login-page/login.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { SignupComponent } from './pages/signup-page/signup.component';
+import { TodayConcertsPageComponent } from './pages/today-concerts-page/today-concerts-page.component';
 import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
@@ -11,7 +14,10 @@ const routes: Routes = [
   { path: 'home', component: HomepageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'concerts', component: AllConcertsPageComponent },
+  { path: 'today-concerts', component: TodayConcertsPageComponent },
   { path: 'main', component: MainPageComponent, canActivate: [AuthGuard] },
+  { path: 'add-concert', component: AddConcertPageComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '/home'},
 ];
 
