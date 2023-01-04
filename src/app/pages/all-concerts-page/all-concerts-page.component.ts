f4c786c0742c172ac7224e9bd5a6e7a8ee7bd41e
Radu-Ioan Mihai <rimihai2001@gmail.com>
2023-01-04 02:31:15 +0200

    All Concerts backend done

--- src/app/pages/all-concerts-page/all-concerts-page.component.ts
+++ src/app/pages/all-concerts-page/all-concerts-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-all-concerts-page',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-concerts-page.component.scss']
 })
 export class AllConcertsPageComponent implements OnInit {
-
-  constructor() { }
+  allConcerts: Concert[];
+  constructor(
+    public afAuth: AngularFireAuth,
+    private db: AngularFireDatabase,
+  ) { }
 
   ngOnInit(): void {
+    this.getStarted();
   }
 
+  async getStarted() {
+    var conc: Concert[];
+    conc = [];
+    await this.getConcerts().then((value) => {
+      conc = value as Concert[];
+    });
+    this.allConcerts = [...conc];
+  }
+
+  getConcerts() {
+    return new Promise((resolve, reject) => {
+      this.db
+        .list('concerts/concerts')
+        .valueChanges()
+        .subscribe((value) => {
+          resolve(value);
+        });
+    });
+  }
+
+}
+
+class Concert {
+  name: string | undefined;
+  description: string | undefined;
+  band_names: [] | undefined;
+  music_genres: [] | undefined;
+  locationName: string | undefined;
+  locationAddress: string | undefined;
+  concertDate: string | undefined;
+  concertEvent: string | undefined;
+  concertTickets: string | undefined;
 }
